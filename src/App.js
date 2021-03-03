3f49b514268dc596daba84c59e312bdcf10ae087
Raja Sabiq <ther.sabiqarfan@gmail.com>
2021-03-03 08:24:54 +0500

    fetch working now percect but it is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  const [search, setSearch] = useState("");
+  const [api, setAPi] = useState("");
+  const getData = (event) => {
+    event.preventDefault();
+    Axios.get(
+      "https://api.unsplash.com/search/photos/?query=" +
+        search +
+        "&client_id=1pdyl6SJQrKM9TKtNR6TkguUwnGY1mbWqIKLWhzF-Zc"
+    ).then((response) => {
+      setPhoto(response.data.results);
+      console.log(photo[0].urls.small);
+    });
+    // setAPi(
+    //   "https://api.unsplash.com/search/photos/?query=" +
+    //     search +
+    //     "&client_id=1pdyl6SJQrKM9TKtNR6TkguUwnGY1mbWqIKLWhzF-Zc"
+    // );
+    // fetch(api)
+    //   .then((respone) => respone.json())
+    //   .then((data) => {
+    //     setPhoto(data.results);
+    //     console.log(photo[0].urls.small);
+    //   });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <h1>{api}</h1>
+        <input
+          type="text"
+          placeholder="Search Category"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={getData}> Search </button>
+      </form>
+      {photo.map((data) => (
+        <img id={data.id} src={data.urls.small} />
+      ))}
     </div>
   );
 }
